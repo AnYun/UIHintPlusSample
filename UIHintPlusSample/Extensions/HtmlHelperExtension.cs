e6688429659252af6f5ffc30b0fb786417c0d162
AnYun <coding@anyun.idv.tw>
2021-01-09 18:32:17 +0800

    Add GetAdditionalValue HtmlHelper

---
+++ UIHintPlusSample/Extensions/HtmlHelperExtension.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UIHintPlusSample
+{
+    /// <summary>
+    /// HtmlHelper Extension
+    /// </summary>
+    public static class HtmlHelperExtension
+    {
+        /// <summary>
+        /// 用指定的 key 去抓 AdditionalValues 中的Value
+        /// </summary>
+        public static string GetAdditionalValue(this IHtmlHelper helper, string key)
+        {
+            return GetAdditionalValue<string>(helper, key, default);
+        }
+        /// <summary>
+        /// 用指定的 key 去抓 AdditionalValues 中的Value
+        /// </summary>
+        public static T GetAdditionalValue<T>(this IHtmlHelper helper, string key)
+        {
+            return GetAdditionalValue<T>(helper, key, default);
+        }
+        /// <summary>
+        /// 用指定的 key 去抓 AdditionalValues 中的Value
+        /// 並在沒有設定時傳回預設值
+        /// </summary>
+        public static T GetAdditionalValue<T>(this IHtmlHelper helper, string key, T defaultValue)
+        {
+            var dic = helper.ViewContext.ViewData.ModelMetadata.AdditionalValues;
+            if (dic != null && dic.ContainsKey(key))
+            {
+                var value = dic[key].ToConvertOrDefault<T>(defaultValue);
+                return value;
+            }
+            return defaultValue;
+        }
+    }
+}
---
+++ UIHintPlusSample/Extensions/ObjectExtension.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UIHintPlusSample
+{
+    /// <summary>
+    /// Object Extension
+    /// </summary>
+    public static class ObjectExtension
+    {
+        /// <summary>
+        /// 轉型或取得預設值
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="obj"></param>
+        /// <param name="defaultValue"></param>
+        /// <returns></returns>
+        public static T ToConvertOrDefault<T>(this object obj, T defaultValue)
+        {
+            try
+            {
+                return (T)Convert.ChangeType(obj, typeof(T));
+            }
+            catch
+            {
+                return defaultValue;
+            }
+        }
+    }
+}
