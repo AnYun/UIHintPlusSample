ebc0b45bcfa95a20ead50ba7e48af7f83fcbd949
AnYun <coding@anyun.idv.tw>
2021-01-09 18:32:50 +0800

    Add sample model and view

---
+++ UIHintPlusSample/Models/LoginViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UIHintPlusSample.Models
+{
+    public class LoginViewModel
+    {
+        /// <summary>
+        /// Account
+        /// </summary>
+        [UIHint("PlaceHolder", "MVC", "PlaceHolderText", "帳號")]
+        public string Account { get; set; }
+        /// <summary>
+        /// Password
+        /// </summary>
+        [UIHint("PlaceHolder", "MVC", "PlaceHolderText", "密碼")]
+        public string Password { get; set; }
+    }
+}
